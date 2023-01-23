76604ad92c995b7022e1b12994b3ceb615cf1e1a
Ro-hitMali <rm4701740@gmail.com>
2023-01-23 19:43:44 +0530

    Fix: Card component

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import classes from './Card.module.css'
 
+ //props.children will give a content which inside Card tag.
+
 const Card = (props) => {
-    //props.children will give a content which inside Card tag.
-    return <div className={`${classes.card} ${props.className}`}> {props.children} </div>;
-}
+   
+    return (<div className={`${classes.card} ${props.className}`}> 
+    {props.children} 
+    </div>
+    )
+};
+
+export default Card;
 
 // classname should be same as card classname. we are merging internally defined class with our external class.
-export default Card;
